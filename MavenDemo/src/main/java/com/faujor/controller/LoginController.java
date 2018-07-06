8247cfed9a181f2e94aa2cce796a7a386c892e41
xiazhitao <xiazhitao@xiazhitao-PC>
2018-07-06 15:01:46 +0800

    第二次提交2018年7月6日

--- MavenDemo/src/main/java/com/faujor/controller/LoginController.java
+++ MavenDemo/src/main/java/com/faujor/controller/LoginController.java
@@ -2,11 +2,13 @@ package com.faujor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class LoginController {
 
+	@ResponseBody
 	@RequestMapping("/login")
 	public String login(){
 		return "登录成功";
